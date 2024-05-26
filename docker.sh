f57f65dd771208ee29582e55abdb68e381a81c0d
mjaya69703 <mjaya69703@gmail.com>
2024-05-26 21:57:10 +0700

    Fix: Init Automatic Setup Docker

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 docker compose up -d --build
 docker compose exec phpmyadmin chmod 777 /sessions
-docker compose exec php bash
+docker compose exec php mkdir storage/framework/views
 docker compose exec php chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 docker compose exec php chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 docker compose exec php composer setup
